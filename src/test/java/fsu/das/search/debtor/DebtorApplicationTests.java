ee035d5cd0b5e9801972c33a2e219ba4bd696608
Cao, Jeff (MCCSS) <jeff.cao@ontario.ca>
2020-03-18 01:31:39 -0400

    first commit

---
+++ src/test/java/fsu/das/search/debtor/DebtorApplicationTests.java
@@ -0,0 +1,13 @@
+package fsu.das.search.debtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
